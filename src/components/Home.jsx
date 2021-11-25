1c34c7aef9f69c0bb9bde36fdc32f16b02863e43
sophielepert <sophie.lepert@gmail.com>
2021-11-25 16:07:41 +0100

    feat create home file and content

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Home(){
+
+return(
+    <div>
+        <h1>Apprennez l'anglais ! </h1>
+        <p>Améliorez votre anglais en vous entraînant grâce à notre jeu musical ! </p>
+         <p>Attention, c'est un jeu sonore, mettez votre haut parleur ou votre casque audio. </p>
+         <p>logo son</p>
+
+        <h2>Règles</h2>
+        <ul>
+            <li>Un extrait de chanson anglaise traduite en français vous sera proposé.</li>
+            <li>Vous aurez 3 propositions de chansons.</li>
+            <li>A vous de trouver la bonne réponse pour découvrir un extrait vidéo</li>
+        </ul>
+        <button>Jouer</button>
+    </div>
+)
+};
+
+export default Home; 
