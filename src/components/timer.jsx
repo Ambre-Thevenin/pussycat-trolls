7932b8588cb80b210251e0cb57329139e0737632
Mel <melissa.olas@gmail.com>
2021-11-26 00:07:41 +0100

    button styled and package autoplay

---
+++ src/components/timer.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+// import styles from './timer.module.css';
+
+export default function Timer() {
+    const [timeLeft, setTimeLeft] = useState(15);
+    const [inter, setInter] = useState()
+
+    useEffect(() => {
+         const interval = setInterval(() => {
+            setTimeLeft((prevState) => prevState - 1);
+        }, 1000);
+        setInter(interval)
+    }, []);
+
+    useEffect(() => {
+        if(timeLeft === 0) {
+            clearInterval(inter)
+        }
+    },[timeLeft])
+
+    return <div>{timeLeft}</div>;
+}
