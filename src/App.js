94dbba8f2ec7d8026688a93fd0a30bb87731d09a
sophielepert <sophie.lepert@gmail.com>
2021-11-26 09:46:14 +0100

    Create end file and fix css issues on score button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import { Routes, Link, Route, useParams } from 'react-router-dom';
 
-import Game from './components/Game'
-import Home from './components/Home'
+import Game from './components/Game';
+import Home from './components/Home';
+import End from './components/End';
+
 import styles from './App.module.css';
 import Logo from './assets/logodiag.png';
 import Footer from './assets/Footer.png';
 import Queen from './assets/queen.png';
+
 
 
 function App() {
@@ -35,6 +38,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
           <Route path ='/:id' element={<Game num={id} setScore={setScore} score={score} setTotal={setTotal} total={total}/>}></Route>
+          <Route exact path='/End' element={<End />}></Route>
         </Routes>
         </div>
       </div>
