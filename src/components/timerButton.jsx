12eefbe40131776e69b1196e7429126e1f5ffa5d
Mel <melissa.olas@gmail.com>
2021-11-26 01:48:51 +0100

    timer game ok

--- src/components/timerButton.jsx
+++ src/components/timerButton.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import Timer from './timer';
 import styles from './timer.module.css';
-import Skip from '../assets/skip_next.svg';
+// import Skip from '../assets/skip_next.svg';
 
 export default function TimerButton() {
-const [startTimer, setStartTimer] = useState(true);
+const [startTimer] = useState(true);
 return (
 <div className={styles.timer} >
           {startTimer  && <Timer />}          
-          <button className={styles.gameButton} onClick={() => setStartTimer((prevState) => !prevState)}><img alt="skip" src={Skip} /> {startTimer}</button>
+          {/* <button className={styles.gameButton} onClick={() => setStartTimer((prevState) => !prevState)}><img alt="skip" src={Skip} /> {startTimer}</button> */}
 </div>
 )
 }
